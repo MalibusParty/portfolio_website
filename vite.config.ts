8144a2c60531cf2f58cc6a81c369b3ac876be10b
Felix Ruf <felix.ruf@student.hs-rm.de>
2022-11-22 00:22:18 +0100

    next fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio_website/',
