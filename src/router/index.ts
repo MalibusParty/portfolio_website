aa8ea42c2f5ef21484fcfb0b7c9ab11e5c0c31e6
Felix Ruf <felix.ruf@student.hs-rm.de>
2022-11-22 11:38:39 +0100

    possible fix for 404 on reload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import TitleView from '@/views/TitleView.vue';
 import ProjectOne from '@/views/ProjectOne.vue';
 import ProjectTwo from '@/views/ProjectTwo.vue';
@@ -7,7 +7,7 @@ import ProjectTwoInfo from '@/views/ProjectTwoInfo.vue';
 import ImpressumView from '@/views/ImpressumView.vue';
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [{
         path: '/',
         name: 'title',
